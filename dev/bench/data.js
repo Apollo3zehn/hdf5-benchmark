fb30a7924dc67d542202e84cfe5fe046a6fc2b0c
github-action-benchmark <github@users.noreply.github.com>
2024-06-14 20:38:43 +0000

    add My Project .NET Benchmark (benchmarkdotnet) benchmark result for 1ac4c94cce3e40143f7b6786ba6014fb1858f876

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1718397523021,
+  "repoUrl": "https://github.com/Apollo3zehn/hdf5-benchmark",
+  "entries": {
+    "My Project .NET Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "git@m1.apollo3zehn.net",
+            "name": "Apollo3zehn"
+          },
+          "committer": {
+            "email": "git@m1.apollo3zehn.net",
+            "name": "Apollo3zehn"
+          },
+          "distinct": true,
+          "id": "1ac4c94cce3e40143f7b6786ba6014fb1858f876",
+          "message": "Fix",
+          "timestamp": "2024-06-14T22:31:25+02:00",
+          "tree_id": "a3767e69b34713719c4fac57d9842a6c375bcd06",
+          "url": "https://github.com/Apollo3zehn/hdf5-benchmark/commit/1ac4c94cce3e40143f7b6786ba6014fb1858f876"
+        },
+        "date": 1718397522696,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.chunked_btree1_no_filter.Run",
+            "value": 683871.8141276041,
+            "unit": "ns",
+            "range": "± 1942.872855361026"
+          }
+        ]
+      }
+    ]
+  }
+}

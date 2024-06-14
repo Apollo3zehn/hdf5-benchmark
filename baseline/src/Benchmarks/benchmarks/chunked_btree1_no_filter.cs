bb8ea007e308641b518465417f92f957b28bb420
Apollo3zehn <git@m1.apollo3zehn.net>
2024-06-14 22:58:04 +0200

    Fix

--- baseline/src/Benchmarks/benchmarks/chunked_btree1_no_filter.cs
+++ baseline/src/Benchmarks/benchmarks/chunked_btree1_no_filter.cs
@@ -7,7 +7,7 @@ namespace Benchmarks;
 [JsonExporterAttribute.Full]
 public class chunked_btree1_no_filter
 {
-    const string FILE_NAME = "chunked_no_filter.h5";
+    const string FILE_PATH = "../../../../../../../../../../data/data/chunked_btree1_no_filter.h5";
 
     private long _fileId = -1;
     private long _datasetId = -1;
@@ -16,15 +16,7 @@ public class chunked_btree1_no_filter
     [GlobalSetup]
     public void GlobalSetup()
     {
-        using (var client = new HttpClient())
-        {
-            using var sourceStream = client.GetStreamAsync("https://raw.githubusercontent.com/hdf5-benchmark/data/main/data/chunked_btree1_no_filter.h5");
-            using var targetStream = new FileStream(FILE_NAME, FileMode.Create);
-
-            sourceStream.Result.CopyTo(targetStream);
-        }
-
-        _fileId = H5F.open(FILE_NAME, H5F.ACC_RDONLY);
+        _fileId = H5F.open(FILE_PATH, H5F.ACC_RDONLY);
 
         if (_fileId < 0)
             throw new Exception("Could not open file");
@@ -41,8 +33,8 @@ public class chunked_btree1_no_filter
         if (H5I.is_valid(_fileId) > 0)
             _ = H5F.close(_fileId);
 
-        if (File.Exists(FILE_NAME))
-            File.Delete(FILE_NAME);
+        if (File.Exists(FILE_PATH))
+            File.Delete(FILE_PATH);
     }
 
     [Benchmark]
--- csharp-purehdf/src/Benchmarks/benchmarks/chunked_btree1_no_filter.cs
+++ csharp-purehdf/src/Benchmarks/benchmarks/chunked_btree1_no_filter.cs
@@ -1,29 +1,29 @@
 using BenchmarkDotNet.Attributes;
 using PureHDF;
+using PureHDF.VOL.Native;
 
 namespace Benchmarks;
 
 [JsonExporterAttribute.Full]
 public class chunked_btree1_no_filter
 {
+    const string FILE_PATH = "../../../../../../../../../../data/data/chunked_btree1_no_filter.h5";
+
+    private NativeFile _h5File = default!;
     private IH5Dataset _dataset = default!;
     private long[] _buffer = new long[1024 * 100];
 
     [GlobalSetup]
     public void GlobalSetup()
     {
-        var fileName = "chunked_no_filter.h5";
+        _h5File = H5File.OpenRead(FILE_PATH);
+        _dataset = _h5File.Dataset("chunked");
+    }
 
-        using (var client = new HttpClient())
-        {
-            using var sourceStream = client.GetStreamAsync("https://raw.githubusercontent.com/hdf5-benchmark/data/main/data/chunked_btree1_no_filter.h5");
-            using var targetStream = new FileStream(fileName, FileMode.Create);
-
-            sourceStream.Result.CopyTo(targetStream);
-        }
-
-        var file = H5File.OpenRead(fileName);
-        _dataset = file.Dataset("chunked");
+    [GlobalCleanup]
+    public void GlobalCleanup()
+    {
+        _h5File.Dispose();
     }
 
     [Benchmark]

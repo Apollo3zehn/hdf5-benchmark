f0b4b80fd57c1bcad70824cb3e64a00f0710f0af
github-action-benchmark <github@users.noreply.github.com>
2024-06-15 10:57:55 +0000

    add chunked_btree1_no_filter_1d (benchmarkdotnet) benchmark result for 1ea4f243de59e7c27a60a11e9d5e351e8471a37f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718397725537,
+  "lastUpdate": 1718449075485,
   "repoUrl": "https://github.com/Apollo3zehn/hdf5-benchmark",
   "entries": {
     "My Project .NET Benchmark": [
@@ -56,6 +56,36 @@ window.BENCHMARK_DATA = {
             "value": 703419.0419921875,
             "unit": "ns",
             "range": "± 3658.409598309801"
+          }
+        ]
+      }
+    ],
+    "chunked_btree1_no_filter_1d": [
+      {
+        "commit": {
+          "author": {
+            "email": "git@m1.apollo3zehn.net",
+            "name": "Apollo3zehn"
+          },
+          "committer": {
+            "email": "git@m1.apollo3zehn.net",
+            "name": "Apollo3zehn"
+          },
+          "distinct": true,
+          "id": "1ea4f243de59e7c27a60a11e9d5e351e8471a37f",
+          "message": "Clean up",
+          "timestamp": "2024-06-15T12:57:19+02:00",
+          "tree_id": "122c2567c7db69311f055baaf5d3f7d4a07053f2",
+          "url": "https://github.com/Apollo3zehn/hdf5-benchmark/commit/1ea4f243de59e7c27a60a11e9d5e351e8471a37f"
+        },
+        "date": 1718449074563,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.chunked_btree1_no_filter_1d.Run",
+            "value": 679147.3837890625,
+            "unit": "ns",
+            "range": "± 1669.9925069478106"
           }
         ]
       }

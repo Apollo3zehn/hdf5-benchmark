e6fd628b7a10303dc2b61e2b562673312ca3a5b0
Apollo3zehn <git@m1.apollo3zehn.net>
2024-06-21 20:52:04 +0200

    Fix incorrect test data

--- data/Program.cs
+++ data/Program.cs
@@ -1,6 +1,11 @@
 ﻿using HDF.PInvoke;
 
-var fileId = H5F.create("data/chunked_new_chunk_indices_no_filter_2d.h5", H5F.ACC_TRUNC);
+var faplId = H5P.create(H5P.FILE_ACCESS);
+var version = H5F.libver_t.V110;
+
+H5P.set_libver_bounds(faplId, version, version);
+
+var fileId = H5F.create("data/chunked_new_chunk_indices_no_filter_2d.h5", H5F.ACC_TRUNC, H5P.DEFAULT, faplId);
 
 TestUtils.AddChunkedDataset_Single_Chunk(fileId);
 TestUtils.AddChunkedDataset_Implicit(fileId);
